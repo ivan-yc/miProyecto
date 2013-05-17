e09c4b0f247dd2bae0e0f7a46b019cab5fa345f6
IVAN-PC <berseck00@gmail.com>
2013-05-16 19:00:41 -0500

    Se agrego la clase CampoBatalla con 2 funciones onclick
    La funcion cambiarBoton, me permite alternar los simbolos de cada jugador('X' y 'O')
    La funcion bReset, resetea el juego terminado

--- TresEnRaya/src/com/example/tresenraya/MainActivity.java
+++ TresEnRaya/src/com/example/tresenraya/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.tresenraya;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -18,6 +20,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void bJugar(View v){
+    	Intent jugar=new Intent(this, CampoBatalla.class);
+    	startActivity(jugar);
     }
     
 }
